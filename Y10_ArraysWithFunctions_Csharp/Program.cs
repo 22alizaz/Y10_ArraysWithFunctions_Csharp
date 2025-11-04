89933a7d114a566a19f2bf8edf1eda414ed6bf12
Aliza Zubair <22AZubair@heckgrammar.co.uk>
2025-11-04 14:11:45 +0000

    .

--- Y10_ArraysWithFunctions_Csharp/Program.cs
+++ Y10_ArraysWithFunctions_Csharp/Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] oneDnames = { "stacy", "keisha", "tory", "lola", "jackie", "daisy", "becky", "shakespeare", "archibald", "pibble" };
+            names(oneDnames);
+            int[] oneDnum = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            integers(oneDnum);
+            string[,] twoDnames = {{ "stacy", "keisha", "tory" }, { "lola", "jackie", "daisy" }, { "shakespeare", "archibald", "pibble" } };
+            list(twoDnames);
+            int[,] twoDnum = { { 1, 2, 3, 4 }, { 5,6,7,8 }, { 9, 10, 11, 12 } };
+            listnum(twoDnum);
+        }
+
+        static void names(string[] name)
+        {
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+        }
+
+        static void integers(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+
+        static void list(string[,] threenames)
+        {
+            for (int i = 0; i < threenames.GetLength(0); i++)
+            {
+                for (int j = 0; j < threenames.GetLength(1); j++)
+                {
+                    Console.Write(threenames[i, j] + ", ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void listnum(int[,] nums)
+        {
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                for (int j = 0; j < nums.GetLength(1); j++)
+                {
+                    Console.Write(nums[i, j] + ", ");
+                }
+            }
         }
     }
 }
