3845e3a340217ebe8a23af0fc8f0116105c3cb54
Aliza Zubair <22AZubair@heckgrammar.co.uk>
2025-11-04 13:20:34 +0000

    Add project files.

---
+++ Y10_ArraysWithFunctions_Csharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Y10_ArraysWithFunctions_Csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
